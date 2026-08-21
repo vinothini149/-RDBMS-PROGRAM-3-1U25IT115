c06200ff07eafb0ec4c9424ffa441c67673bec98
vinothini149 <vinothiniav1409@gmail.com>
2026-08-21 21:28:54 +0530

    PROGRAM 3

--- solution.sql
+++ solution.sql
@@ -1,1 +1,16 @@
+CREATE DATABASE collegeDB;
+USE collegeDB;
+CREATE TABLE department(
+  studentID VARCHAR(5)PRIMARY KEY,
+  studentName VARCHAR(10)NOT NULL,
+  DOB date NOT NULL,
+  gender VARCHAR(10)NOT NULL
+  );
+desc department;
+ALTER TABLE student
+ADD(
+  Email VARCHAR(30)NOT NULL,
+  PhoneNumber INT(10)NOT NULL
+);
+desc student;
 
